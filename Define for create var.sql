dcebb04e6f062509fa48008e81f7df1d29bbd636
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-22 16:57:49 -0400

    Update Define for create var.sql

--- Define for create var.sql
+++ Define for create var.sql
@@ -8,3 +8,16 @@ SQL> SELECT cedula, apellido
 SQL> UNDEFINE ced_empleado
 
 SHOW ALL    -- muestra uma lista con todas las variables
+
+
+-- Ejemplo de variables de substitución 
+-- • Para strings y fechas, ponga la variable entre apóstrofes
+-- • Use (&&) si se desea reutilizar el valor de la variable sin tener que 
+-- repetir el valor por cada ejecucion
+SQL> SELECT nombre ||' '|| apellido as Empleado
+2 FROM b_empleados
+3 WHERE cedula = &c;
+
+SQL> SELECT nombre ||' '|| apellido as Empleado
+2 FROM b_empleados
+3 WHERE apellido = '&apellido';
