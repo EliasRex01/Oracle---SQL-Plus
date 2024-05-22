23940e7c21df809f1368a6e30e5dd9c103c2f4c5
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-22 16:59:38 -0400

    Create Comando COLUMN : Ejemplos.sql

---
+++ Comando COLUMN : Ejemplos.sql
@@ -0,0 +1,13 @@
+
+• Seteando columnas con heading
+• Desplegando el seteo para la columna APELLIDO
+• Limpiar seteos para la columna APELLIDO
+COLUMN apellido HEADING 'Nombre| Empleado' FORMAT A15
+COLUMN asignacion JUSTIFY LEFT FORMAT $99,990.00
+COLUMN fecha_ing FORMAT A10 NULL ‘No contratado'
+
+--  Desplegando el seteo para la columna APELLIDO
+COLUMN apellido
+
+-- Limpiar seteos para la columna APELLIDO
+COLUMN apellido CLEAR
