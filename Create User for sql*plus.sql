e83d9023e60d7fd7e17fa9830254941be6b81cd5
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-07-18 10:57:31 -0400

    Update Create User for sql*plus.sql

--- Create User for sql*plus.sql
+++ Create User for sql*plus.sql
@@ -14,7 +14,7 @@ Usuario modificado.
 SQL> GRANT CREATE SESSION TO bd2_rex;                  -- asignar inicio de sesion
 Concesi≤n terminada correctamente.
 SQL> GRANT "RESOURCE" TO bd2_rex;                  -- asignar rol resource a usuario
-Concesi≤n terminada correctamente.
+Concesion terminada correctamente.
 SQL> ALTER USER bd2_rex DEFAULT ROLE "RESOURCE";         -- habilitar rol por defecto como resource para el usuario
 Usuario modificado.
 
