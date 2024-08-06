f9ded6302dda17ba89fd2246af86b0528954a2c4
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-08-06 19:31:17 -0400

    Update Create User for sql*plus.sql

--- Create User for sql*plus.sql
+++ Create User for sql*plus.sql
@@ -24,3 +24,27 @@ USER es bd2_rex
 CON_NAME
 ................................................
 CDB$RESOURCE
+
+
+
+-- Para las clases y examenes de bd2
+SQL> CREATE USER basedatos2 IDENTIFIED BY "basedatos2"      -- crear usuario
+  2  DEFAULT TABLESPACE "USERS"
+  3  TEMPORARY TABLESPACE "TEMP"
+  4  ;
+Usuario creado.
+SQL> ALTER USER basedatos2 QUOTA UNLIMITED ON USERS;      -- asignar uso ilimitado del espacio
+Usuario modificado.
+SQL> GRANT CREATE SESSION TO basedatos2;                  -- asignar inicio de sesion
+Concesi≤n terminada correctamente.
+SQL> GRANT "RESOURCE" TO basedatos2;                  -- asignar rol resource a usuario
+Concesion terminada correctamente.
+SQL> ALTER USER basedatos2 DEFAULT ROLE "RESOURCE";         -- habilitar rol por defecto como resource para el usuario
+Usuario modificado.
+
+-- ConectandoIntroduzca el nombre de usuario: basedatos2/basedatos2
+Conectado a:
+Oracle Database 21c Express Edition Release 21.0.0.0.0 - Production
+Version 21.3.0.0.0
+SQL> ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;     -- permitir alteraciones
+Sesion modificada.
