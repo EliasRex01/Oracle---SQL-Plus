694ab5eb25ac271ee8e1f76564906cd4dbd1f0d3
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-08-06 19:45:46 -0400

    Create Comandos utiles.sql

---
+++ Comandos utiles.sql
@@ -0,0 +1,7 @@
+-- comando get para obtener el contenido de un archivo en concreto
+SQL> get test.sql
+
+-- comando edit permite abrir el blog de notas para editar el contenido de un archivo
+-- que se ejecuto con un select
+SQL> edit test.sql
+
