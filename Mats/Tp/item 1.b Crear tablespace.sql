8344fb54af37bbc935faa2efeea267c93c266018
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-08-23 19:17:21 -0400

    Update item 1.b Crear tablespace.sql

--- Mats/Tp/item 1.b Crear tablespace.sql
+++ Mats/Tp/item 1.b Crear tablespace.sql
@@ -14,9 +14,6 @@ DROP TABLESPACE BASEHOTELTP
   [INCLUDING CONTENTS AND DATAFILES [CASCADE CONSTRAINTS]];
 
 
-
-
-
 -- El siguiente segmento determina el directorio por default para la creacion del datafile:
 ALTER SYSTEM SET DB_CREATE_FILE_DEST = 
 '$ORACLE_HOME/rdbms/dbs';
@@ -24,28 +21,3 @@ ALTER SYSTEM SET DB_CREATE_FILE_DEST =
 -- Posteriormente los tablespaces se generaran en el destino definido:
 CREATE TEMPORARY TABLESPACE temp_demo TEMPFILE 
 'temp01.dbf' SIZE 5M AUTOEXTEND ON;
-
--- Creacion de un segmento de rollback
-CREATE [PUBLIC] ROLLBACK SEGMENT <nombre>
-[TABLESPACE nombre]
-[STORAGE (clausula de storage)]:
-
--- Creacion de un segmento de rollback
-CREATE ROLLBACK SEGMENT rbs01
-TABLESPACE rbs
-STORAGE (INITIAL 100K NEXT 100K OPTIMAL 4M
-MINEXTENTS 20 MAXEXTENTS 100);
-
--- Se habilita el rollback segment
-ALTER ROLLBACK SEGMENT RB_TEMP ONLINE;
-
--- Cambio de asignacion de tablespace por defecto y tablespace temporal para el usuario SYSTEM.
-ALTER USER SYSTEM TEMPORARY TABLESPACE TEMPORARY_DATA;
-ALTER USER SYSTEM DEFAULT TABLESPACE USER_DATA;
-
--- Creacion de rollback segment publico (Estimar la cantidad de segmentos de
--- rollback de acuerdo a los procesos ejecutados y a los usuarios que son conectados.
--- Se recomienda un segmento de rollback por cada 4 (cuatro) usuarios conectados)
-CREATE PUBLIC ROLLBACK SEGMENT RB1 STORAGE(INITIAL 
-50K NEXT 50K)
-TABLESPACE ROLLBACK_DATA;
