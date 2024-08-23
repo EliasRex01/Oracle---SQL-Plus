06bfc5ac948e72fb7f8e379dee6de9113151056c
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-08-23 19:07:39 -0400

    Update item 1.b Crear tablespace.sql

--- Mats/Tp/item 1.b Crear tablespace.sql
+++ Mats/Tp/item 1.b Crear tablespace.sql
@@ -2,15 +2,19 @@
 
 CREATE TABLESPACE BASEHOTELTP
   DATAFILE '/ORANT/ORADATA/BASED2_01.dbf' SIZE 50M
+  AUTOEXTEND ON NEXT 10M MAXSIZE UNLIMITED
   DEFAULT STORAGE (INITIAL 10K NEXT 50K
     MAXEXTENTS 100
     PCTINCREASE 0)
   ONLINE;
-  
 
--- Eliminacion de un tablespace
-DROP TABLESPACE <espacio de tablas>
-  [INCLUDING CONTENTS [CASCADE CONSTRAINTS]];
+
+-- Eliminar el tablespace
+DROP TABLESPACE BASEHOTELTP
+  [INCLUDING CONTENTS AND DATAFILES [CASCADE CONSTRAINTS]];
+
+
+
 
 
 -- El siguiente segmento determina el directorio por default para la creacion del datafile:
