925c83987c1b5b9473cf1cc827e6b682d5878d7a
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-10-17 13:26:00 -0300

    Update PCK_HOTEL.sql

--- TP/PCK_HOTEL.sql
+++ TP/PCK_HOTEL.sql
@@ -1,11 +1,12 @@
-El tipo de dato T_HABITACIONES como una tabla indexada que tendrá los atributos:
-o CATEGORIA VARCHAR2(100)
-o COSTO_X_NOCHE NUMBER(10)
- La función F_VERIFICAR_DISPONIBILIDAD que recibirá como parámetros: La identificación del 
+
+ La función F_VERIFICAR_DISPONIBILIDAD 
+ que recibirá como parámetros: La identificación del 
 hotel, cantidad de huéspedes, fecha_desde y fecha_hasta; y devolverá una variable del tipo 
 T_HABITACIONES.
- El procedimiento P_FACTURAR que recibirá como parámetro la identificación del hotel
- El procedimiento P_CONSULTAR_OCUPACION que recibirá como parámetros: Criterio y valor, 
+ El procedimiento P_FACTURAR que recibirá 
+ como parámetro la identificación del hotel
+ El procedimiento P_CONSULTAR_OCUPACION 
+ que recibirá como parámetros: Criterio y valor, 
 ambos definidos de tipo VARCHAR2.
 
 
