552976df0c0655b3440883ea87b23f1468946ba1
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-10-20 08:49:32 -0300

    Update PCK_HOTEL.sql

--- TP/PCK_HOTEL.sql
+++ TP/PCK_HOTEL.sql
@@ -16,6 +16,28 @@ END;
 
 
 -- body del paquete
+La función F_VERIFICAR_DISPONIBILIDAD deberá validar que la cantidad de
+huéspedes esté entre 1 y 3 atendiendo a la capacidad de las habitaciones del hotel.
+Cualquier otro valor será rechazado. La fecha inicial de la reserva no puede ser
+anterior a la fecha de hoy. Además, las fecha_hasta debe ser mayor a la
+fecha_desde. Con los parámetros recibidos deberá seleccionar todas las
+habitaciones con la capacidad solicitada y que estén disponibles, es decir que no
+posean reservas Confirmadas o Pendientes en el rango de fechas recibido por
+parámetro. Debe considerar que si la fecha_desde coincide con la fecha de
+checkout de una reserva (confirmada o pendiente) se la podrá considerar también
+disponible. Con los datos seleccionados la función deberá cargar la variable del tipo
+T_HABITACIONES indexando por el número de habitación
+
+
+FUNCTION F_VERIFICAR_DISPONIBILIDAD(La identificación del 
+hotel, cantidad de huéspedes, fecha_desde y fecha_hasta) RETURN T_HABITACIONES;
+
+
+
+
+
+
+
 
 
 
